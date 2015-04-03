dda372d90a374134df8ae1d24a43fec16fb1d7fc
Bryan Clark <clarkbw@gmail.com>
2015-04-03 07:23:05 -0700

    jscs fixes for the email script

--- server/boot/email.js
+++ server/boot/email.js
@@ -4,33 +4,37 @@ var path = require('path');
 
 module.exports = function(app) {
   if (app.dataSources.db.name !== 'Memory') { return; }
+
   var file = path.resolve(__dirname, '..', '..', 'client', 'email.html');
   var User = app.models.User;
 
   // clear out the email file
-  fs.writeFile(file, '', { flag: 'w+' }, function (err) {
+  fs.writeFile(file, '', { flag: 'w+' }, function(err) {
     if (err) { console.error(err); }
   });
 
   // demo trigger method to run the verify function
   User.trigger = function(to, from, subject, next) {
-    var options = { type : 'email',
-                    to : { address : to },
-                    from : { address : from },
-                    subject : subject,
+    var options = { type: 'email',
+                    to: { address: to },
+                    from: { address: from },
+                    subject: subject,
                     cache: false };
-    User.findOne({}, function (err, user) {
+    User.findOne({}, function(err, user) {
       // overwrite the current file
-      fs.writeFile(file, '', { flag: 'w+' }, function (err) {
+      fs.writeFile(file, '', { flag: 'w+' }, function(err) {
         if (err) { console.error(err); }
       });
 
-      user.verify(options, function (err, result) {
+      user.verify(options, function(err, result) {
         if (err) { console.error(err); }
+
         var msg = result.email.html.toString('utf-8');
+
         // write out the new email so it can be loaded in an iframe
-        fs.writeFile(file, msg, { flag: 'w+' }, function (err) {
+        fs.writeFile(file, msg, { flag: 'w+' }, function(err) {
           if (err) { console.error(err); }
+
           next(null, result);
         });
       });
@@ -56,7 +60,7 @@ module.exports = function(app) {
                email: 'clarkbw@example.com',
                password: '123456789'
               };
-  User.upsert(data, function (err, user) {
+  User.upsert(data, function(err, user) {
     console.log('Created a default user', user);
   });
 };
